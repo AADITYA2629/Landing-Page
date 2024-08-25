2ea2915d2ae24b1b464fb24ebc13e01aee4c2737
AADITYA_RP <150111578+AADITYA2629@users.noreply.github.com>
2024-08-25 20:28:37 +0530

    Add files via upload
    A Product-Based Landing page is made with HTML ,CSS & JS. It Includes header section, body section & footer section with images.

---
+++ script.js
@@ -0,0 +1,9 @@
+function imgsilder(anything)
+{
+    document.querySelector('.pepsi').src=anything;
+}
+function changeBColor(color)
+{
+    const sec =document.querySelector('.sec');
+    sec.style.background=color;
+}
